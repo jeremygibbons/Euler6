3907c5e04fa9d82525c64bd5400044c4d52808d8
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-08-27 06:01:59 +0200

    Initial Valid Solution

---
+++ Euler6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n;
+            uint.TryParse(args[0], out n);
+
+            ulong s = SumOfIntegers(n);
+
+            s = s * s - SumOfSquares(n);
+
+            Console.WriteLine(s);
+            Console.ReadLine();          
+
+        }
+
+        static ulong SumOfSquares(ulong n)
+        {
+            return n * (n + 1) * (2*n + 1) / 6;
+        }
+
+        static ulong SumOfIntegers(ulong n)
+        {
+            return n * (n + 1) / 2;
+        }
+    }
+}
